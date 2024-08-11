17558a648d8578c02fcc09301ca776fb3f354140
Mohammad Haris <mohdharis010@gmail.com>
2024-08-12 00:25:24 +0530

    Tailwind setup completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        "./index.html",
         "./src/**/*.{js,jsx,ts,tsx}",
-        "./src/components/**/*.{js,jsx,ts,tsx}",
         "./node_modules/flowbite/**/*.js"
     ],
     theme: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), vercel()],
+    plugins: [react()],
     server: {
         port: process.env.PORT as unknown as number,
     },
