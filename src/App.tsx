c3be700239dbcbca65660eb89a027ff8c7083a36
Mohammad Haris <mohdharis010@gmail.com>
2024-08-25 23:02:27 +0530

    Google analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Suspense } from "react";
 import "./App.css";
 import AppRoutes from "@routes/AppRoutes";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize(import.meta.env.VITE_GA_ID);
 function App() {
     return (
         <Suspense>
